e2ac37eddf0a2c3edcac32469bd5987a6d9a7706
ZX <xiaoxiao_6677@foxmail.com>
2023-02-02 16:16:03 +0800

    update installation

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 type wget && \
 type unzip && \
 type go && \
-wget https://github.com/jing332/tts-server-go/archive/refs/heads/master.zip --no-check-certificate && \
+wget https://github.com/jing332/tts-server-go/archive/refs/heads/master.zip || \
+wget https://gh.con.sh/https://github.com/jing332/tts-server-go/archive/refs/heads/master.zip || \
+wget https://archive.fastgit.org/jing332/tts-server-go/archive/refs/heads/master.zip || \
+wget https://ghproxy.com/https://github.com/jing332/tts-server-go/archive/refs/heads/master.zip && \
 unzip master.zip && \
 rm master.zip && \
 cd tts-server-go-master  && \
 mkdir edgetts-cli && \
 cd edgetts-cli && \
-wget https://github.com/ZX-11/edgetts-cli/raw/main/edgetts.go --no-check-certificate && \
+wget https://github.com/ZX-11/edgetts-cli/raw/main/edgetts.go || \
+wget https://cdn.staticaly.com/gh/ZX-11/edgetts-cli/main/edgetts.go || \
+wget https://fastly.jsdelivr.net/gh/ZX-11/edgetts-cli@main/edgetts.go || \
+wget https://gcore.jsdelivr.net/gh/ZX-11/edgetts-cli@main/edgetts.go || \
+wget https://raw.fastgit.org/ZX-11/edgetts-cli/main/edgetts.go || \
+wget https://ghproxy.com/https://github.com/ZX-11/edgetts-cli/raw/main/edgetts.go && \
 go build -ldflags="-s -w" edgetts.go && \
 echo "All Done. Build file can be found in ./tts-server-go-master/edgetts-cli"
